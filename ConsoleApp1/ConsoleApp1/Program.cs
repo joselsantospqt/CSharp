df1b528f7243dab13d99bc9b0b20f4533fffdaa6
José Ricardo Lima dos Santos Pereira <ricardopqt@yahoo.com.br>
2021-04-26 20:02:16 -0300

    Primeiro Exercícios, aprendizado.
    Primeiro Exercícios, aprendizado.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var quantidade = args.Length;
+            int somatorio = 0;
+            int i = 0;
+
+            while (i < quantidade)
+            {
+                if (i >= quantidade)
+                { break; }
+                somatorio = somatorio + Convert.ToInt32(args[i]);
+
+                i++;
+            }
+
+            Console.WriteLine(somatorio);
+        }
+    }
+}
